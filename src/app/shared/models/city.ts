6e126e16140a8514c7d65b946bb2a451a226dcc4
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-20 02:42:00 +0300

    Add some static data for greek cities

---
+++ src/app/shared/models/city.ts
@@ -0,0 +1,11 @@
+export class City {
+    city: string;
+    admin: string;
+    country: string;
+    population_proper: number;
+    iso2: string;
+    capital: string;
+    lat: number;
+    lng: number;
+    population: number;
+}
