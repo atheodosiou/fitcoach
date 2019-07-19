4137469dd1841df9f92dc2583e9d1d9c15c19dc9
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-20 02:11:13 +0300

    Add sdk service, _variables.scss and update project structure

---
+++ src/app/shared/services/sdk.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class Sdk {
+
+  constructor(private http: HttpClient) { }
+
+  get(endpoint: string, httpHeaders?: HttpHeaders): Observable<any> {
+    if (httpHeaders) {
+      return this.http.get(endpoint,{headers:httpHeaders});
+    } else {
+      return this.http.get(endpoint);
+    }
+
+  }
+}
--- src/app/views/welcome/welcome.module.ts
+++ src/app/views/welcome/welcome.module.ts
@@ -1,16 +1,23 @@
 
-import { NgModule } from '@angular/core';
+import { NgModule, Input } from '@angular/core';
 import { WelcomeComponent } from './welcome.component';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button'
 
 @NgModule({
     declarations: [
         WelcomeComponent
     ],
-    imports: [],
+    imports: [
+        CommonModule,
+        FormsModule,
+        InputTextModule,
+        ButtonModule
+    ],
     exports: [
         WelcomeComponent,
-        ScrollToModule
     ]
 })
 export class WelcomeModule{}
