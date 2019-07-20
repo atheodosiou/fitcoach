18c05f4a5961410061ebd6b59ae653e693612657
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-21 01:22:40 +0300

    Modify routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './views/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WelcomeComponent},
+  {path:'trainer-signup',component:WelcomeComponent},
+  {path:'find-personal-trainers',component:WelcomeComponent},
+  {path:'**',component:WelcomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    //Lazy routing
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule],
+  declarations:[WelcomeComponent]
 })
 export class AppRoutingModule { }
