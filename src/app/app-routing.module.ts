fc113de4db1ec74494c7df84ed98ca80d17d7b96
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-21 01:59:04 +0300

    Update project structure, and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './views/welcome/welcome.component';
-import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { TrainerSignupComponent } from './views/trainer-signup/trainer-signup.component';
 import { FindTrainersComponent } from './views/find-trainers/find-trainers.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
+
+// const routes: Routes = [
+//   {
+//     path:'',
+//     loadChildren: () => import('./views/welcome/welcome.module').then(mod => mod.WelcomeModule)
+//   },
+//   {
+//     path:'trainer-signup',
+//     loadChildren: () => import('./views/trainer-signup/trainer-signup.module').then(mod => mod.TrainerSignupModule)
+//   },
+//   {
+//     path:'find-personal-trainers',
+//     loadChildren: () => import('./views/find-trainers/find-trainers.module').then(mod => mod.FindTrainersModule)
+//   },
+//   {
+//     path:'**',
+//     loadChildren: () => import('./views/page-not-found/page-not-found.module').then(mod => mod.PageNotFoundModule)
+//   }
+// ];
 
 const routes: Routes = [
-  {path:'',component:WelcomeComponent},
-  {path:'trainer-signup',component:TrainerSignupComponent},
-  {path:'find-personal-trainers',component:FindTrainersComponent},
-  {path:'**',component:PageNotFoundComponent}
+  {
+    path:'',component:WelcomeComponent
+  },
+  {
+    path:'trainer-signup',component:TrainerSignupComponent
+  },
+  {
+    path:'find-personal-trainers',component:FindTrainersComponent
+  },
+  {
+    path:'**',component:PageNotFoundComponent
+  }
 ];
-
 @NgModule({
   imports: [
     //Lazy routing
-    RouterModule.forChild(routes)
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
-  declarations:[WelcomeComponent,TrainerSignupComponent,FindTrainersComponent,PageNotFoundComponent]
+  declarations:[TrainerSignupComponent,FindTrainersComponent,PageNotFoundComponent]
 })
 export class AppRoutingModule { }
