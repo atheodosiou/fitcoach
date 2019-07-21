21d5758878562ed6ca055596e2f6cedf8c7715a3
atheodosiou <anastasios.theodosiou@gmail.com>
2019-07-21 04:13:42 +0300

    Start to build trainer signup form

--- src/app/views/trainer-signup/trainer-signup.component.ts
+++ src/app/views/trainer-signup/trainer-signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-trainer-signup',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerSignupComponent implements OnInit {
 
+  gender:SelectItem[];
   constructor() { }
 
   ngOnInit() {
+    this.gender=[
+      {label:'Male', value:'MALE'},
+      {label:'Female', value:'FEMALE'},
+      {label:'Neutral', value:'NEUTRAL'},
+      {label:'Rather not say', value:'RATHER_NOT_SAY'}
+    ];
   }
 
 }
--- src/app/views/trainer-signup/trainer-signup.module.ts
+++ src/app/views/trainer-signup/trainer-signup.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { TrainerSignupComponent } from './trainer-signup.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
     declarations:[TrainerSignupComponent],
     exports:[TrainerSignupComponent],
     imports:[
         CommonModule,
-        FormsModule
+        FormsModule,
+        InputTextModule,
+        DropdownModule
     ]
 })
 export class TrainerSignupModule{}
